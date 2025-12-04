d4a29007e4bf7b3efc485d6a4e1805abe72b27ce
woominp <id+woominp@users.noreply.github.com>
2025-12-04 14:13:04 +0900

    FakeViewControllerContainer에 CreateManager 구현 및 테스트를 위해 랜덤 문자열 생성 기능 추가

--- src/main/java/org/example/view/viewcontroller/fake/FakeViewControllerContainer.java
+++ src/main/java/org/example/view/viewcontroller/fake/FakeViewControllerContainer.java
@@ -1,10 +1,12 @@
 package org.example.view.viewcontroller.fake;
 
 import org.example.view.model.Manager;
+import org.example.view.viewcontroller.CreateManager;
 import org.example.view.viewcontroller.GetManagerList;
 import org.example.view.viewcontroller.ViewControllerContainer;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class FakeViewControllerContainer implements ViewControllerContainer {
 
@@ -17,5 +19,27 @@ public class FakeViewControllerContainer implements ViewControllerContainer {
     @Override
     public GetManagerList getManagerList() {
         return () -> managers;
+    }
+
+    @Override
+    public CreateManager createManager() {
+        return (name, grade) -> {
+            managers.addFirst(new Manager(
+                    getRandomString(6), name, grade
+            ));
+        };
+    }
+
+    private String getRandomString(int targetStringLength) {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        var random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        return buffer.toString();
     }
 }
