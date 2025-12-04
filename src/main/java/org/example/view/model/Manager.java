bf800800d4ceddaeb82beca99c7168a91c4aa6ac
woominp <id+woominp@users.noreply.github.com>
2025-12-04 13:15:00 +0900

    뷰 컨트롤러 및 관리자를 가져오는 인터페이스 추가

---
+++ src/main/java/org/example/view/model/Manager.java
@@ -0,0 +1,8 @@
+package org.example.view.model;
+
+public record Manager(
+        String id,
+        String name,
+        String grade
+) {
+}
---
+++ src/main/java/org/example/view/viewcontroller/GetManagerList.java
@@ -0,0 +1,9 @@
+package org.example.view.viewcontroller;
+
+import org.example.view.model.Manager;
+
+import java.util.List;
+
+public interface GetManagerList {
+    List<Manager> execute();
+}
