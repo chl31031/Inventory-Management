f132fec3f0589b877aa2feda462a3216147254c7
galmaeki <kjh09800@gmail.com>
2025-12-05 17:52:31 +0900

    Feat: 뷰 컨트롤러 일부 작성

---
+++ src/main/java/org/example/view/viewcontroller/db/DBViewControllerContainer.java
@@ -0,0 +1,90 @@
+package org.example.view.viewcontroller.db;
+
+import org.example.db.CategoryInterfaceImpl;
+import org.example.db.ItemInterfaceImpl;
+import org.example.db.ManagerInterfaceImpl;
+import org.example.dto.FilteredKeyword;
+import org.example.view.model.Category;
+import org.example.view.model.Item;
+import org.example.view.model.Manager;
+import org.example.view.viewcontroller.*;
+
+public class DBViewControllerContainer implements ViewControllerContainer {
+
+    @Override
+    public GetManagerList getManagerList() {
+        return () ->
+                ManagerInterfaceImpl.getInstance().getManagers().stream().map(it ->
+                        new Manager(it.id(), it.name(), it.grade())
+                ).toList();
+    }
+
+    @Override
+    public CreateManager createManager() {
+        return (name, grade) ->
+                ManagerInterfaceImpl.getInstance().createManager(
+                        new org.example.dto.CreateManager(name, grade)
+                );
+    }
+
+    @Override
+    public GetItemList getItemList() {
+        return (page, search, categoryID) -> {
+            boolean keywordIsNull = search == null || search.isBlank();
+            boolean categoryIsNull = categoryID == null || categoryID.isBlank();
+            if (keywordIsNull && categoryIsNull) {
+                return ItemInterfaceImpl.getInstance().getItems(page).stream()
+                        .map(this::toModel).toList();
+            }
+            if (!keywordIsNull && categoryIsNull) {
+                return ItemInterfaceImpl.getInstance().searchItems(search, page)
+                        .stream().map(this::toModel).toList();
+            }
+            if (!keywordIsNull && !categoryIsNull) {
+                return ItemInterfaceImpl.getInstance().searchFilteredItems(new FilteredKeyword(categoryID, search), page)
+                        .stream().map(this::toModel).toList();
+            }
+            return ItemInterfaceImpl.getInstance().filteredItems(categoryID, page)
+                    .stream().map(this::toModel).toList();
+        };
+    }
+
+    @Override
+    public CreateCategory createCategory() {
+        return (category) ->
+                CategoryInterfaceImpl.getInstance().createCategory(category);
+    }
+
+    @Override
+    public GetCategoryList getCategoryList() {
+        return () -> CategoryInterfaceImpl.getInstance().getCategories().stream()
+                .map(c -> new Category(c, c)).toList();
+    }
+
+    @Override
+    public CreateItem createItem() {
+        return (name, id) -> ItemInterfaceImpl.getInstance().createItem(
+                new org.example.dto.CreateItem(name, id));
+    }
+
+    @Override
+    public GetItem getItem() {
+        return (id) -> {
+            var itemDto = ItemInterfaceImpl.getInstance().getItemById(id);
+
+            return new Item(
+                    itemDto.id(), itemDto.name(), itemDto.category(),
+                    itemDto.category(), itemDto.quantity()
+            );
+        };
+    }
+
+    @Override
+    public GetItemIOList getItemIOList() {
+        return null;
+    }
+
+    private Item toModel(org.example.dto.Item i) {
+        return new Item(i.id(), i.name(), i.category(), i.category(), i.quantity());
+    }
+}
