2355f1c4015230046b51dd2882202e54b6408d96
woominp <245330617+woominp@users.noreply.github.com>
2025-12-05 11:17:06 +0900

    카테고리를 선택할 수 있는 CategorySelectScreen 화면 추가

---
+++ src/main/java/org/example/view/screen/CategorySelectScreen.java
@@ -0,0 +1,43 @@
+package org.example.view.screen;
+
+import org.example.Main;
+import org.example.view.model.Category;
+import org.example.view.viewcontroller.GetCategoryList;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class CategorySelectScreen extends Screen {
+
+    private final GetCategoryList getCategoryList = Main.viewControllerContainer.getCategoryList();
+    private List<Category> categoryList = null;
+
+    private void printCategories() {
+        var screenText = new StringBuilder();
+        for (var i = 0; i < categoryList.size(); i++) {
+            var category = categoryList.get(i);
+            screenText.append(i + 1).append(". ");
+            screenText.append(category.name()).append(' ');
+        }
+        screenText.append("(숫자: 카테고리 선택)");
+        System.out.println(screenText);
+    }
+
+    @Override
+    public void action() {
+        categoryList = getCategoryList.execute();
+        if (categoryList == null) {
+            return;
+        }
+
+        printCategories();
+        var scanner = new Scanner(System.in);
+        var selected = scanner.nextLine();
+        var selectedInt = Integer.parseInt(selected) - 1;
+        if (selectedInt >= 0 && selectedInt < categoryList.size()) {
+            Main.screens.removeLast();
+            Main.screens.getLast().setResult(ResultKey.CATEGORY_ID, categoryList.get(selectedInt).id());
+            return;
+        }
+    }
+}
